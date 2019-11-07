8ea8cc1ba2d34b8196ada33f06dc5310f01b073f
Salman <salman.rahman515@gmail.com>
2019-11-07 02:24:58 -0500

    Lab3: fixed a typo with missing others=>

--- next_address.vhd
+++ next_address.vhd
@@ -20,7 +20,7 @@ architecture next_address_arch of next_address is
     begin
 
         on_branch_sign_ext(target_address'length-1-(5+5) downto target_address'right) <= target_address(target_address'length-1-(5+5) downto target_address'right);
-        on_branch_sign_ext(next_pc'length-1 downto target_address'length-(5+5)) <= target_address(target_address'length-1-(5+5));
+        on_branch_sign_ext(next_pc'length-1 downto target_address'length-(5+5)) <= (other => target_address(target_address'length-1-(5+5)));
 
 
         branch_select: process(on_branch_sign_ext, rs, rt, branch_type)

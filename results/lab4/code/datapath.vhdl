4aa0313159b58880626b8100849faab3d5078ee1
Salman <salman.rahman515@gmail.com>
2019-11-23 17:52:15 -0500

    Lab4: Added all components in the lab 4 code folder

--- results/lab4/code/datapath.vhdl
+++ results/lab4/code/datapath.vhdl
@@ -4,45 +4,42 @@ USE ieee.numeric_std.ALL;
 
 ENTITY datapath IS
     PORT (
-        -- pc ports 
+
+        -- global signal
         reset : IN std_logic;
         clk : IN std_logic;
-        pc_q: OUT std_logic_vector(31 DOWNTO 0);
-        
-        -- i-cache ports
-        icache_instr : OUT std_logic_vector(31 DOWNTO 0);
-        
+    
         -- next address ports
         pc_sel : IN std_logic_vector(1 DOWNTO 0);
         branch_type : IN std_logic_vector(1 DOWNTO 0);
-        next_pc : OUT std_logic_vector(31 DOWNTO 0);
 
         -- reg_des MUX ports
         reg_dst : IN std_logic;
-        add : OUT std_logic;
 
         -- sign extend ports
         func : IN std_logic_vector(1 DOWNTO 0);
-        sign_extnd_out : OUT std_logic_vector(31 DOWNTO 0);
-        
+
         -- regfile ports block
         reg_write : IN std_logic;
-        out_a : OUT std_logic_vector(31 DOWNTO 0);
-        out_b : OUT std_logic_vector(31 DOWNTO 0);
 
         -- alu_src MUX ports
         alu_src : IN std_logic;
-
-
+        
+        -- alu ports
         add_sub : IN std_logic;
-
-
-
         logic_func : IN std_logic_vector(1 DOWNTO 0);
-        data_write : IN std_logic;
-        reg_in_src : IN std_logic;
-        rt_output : OUT std_logic_vector(31 DOWNTO 0);
+        func : IN std_logic_vector(1 DOWNTO 0);
+
         overflow : OUT std_logic;
         zero : OUT std_logic
+
+
+        -- d-cache ports
+        data_write : IN std_logic;
+
+        -- reg_in_src MUX port
+        reg_in_src : IN std_logic;
+        
+
     );
 END datapath;

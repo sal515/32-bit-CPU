5d5eac468441e536a4b9a51fa29bf3707899d286
Salman <salman.rahman515@gmail.com>
2019-11-23 23:09:13 -0500

    Lab4: added components d_cache, i_cache, mux, pc_register and updated datapath

--- results/lab4/code/datapath.vhdl
+++ results/lab4/code/datapath.vhdl
@@ -4,11 +4,10 @@ USE ieee.numeric_std.ALL;
 
 ENTITY datapath IS
     PORT (
-
-        -- global signal
+        -- global signals
         reset : IN std_logic;
         clk : IN std_logic;
-    
+
         -- next address ports
         pc_sel : IN std_logic_vector(1 DOWNTO 0);
         branch_type : IN std_logic_vector(1 DOWNTO 0);
@@ -24,7 +23,7 @@ ENTITY datapath IS
 
         -- alu_src MUX ports
         alu_src : IN std_logic;
-        
+
         -- alu ports
         add_sub : IN std_logic;
         logic_func : IN std_logic_vector(1 DOWNTO 0);
@@ -33,13 +32,99 @@ ENTITY datapath IS
         overflow : OUT std_logic;
         zero : OUT std_logic
 
-
         -- d-cache ports
         data_write : IN std_logic;
 
         -- reg_in_src MUX port
         reg_in_src : IN std_logic;
-        
-
     );
 END datapath;
+
+ARCHITECTURE datapath_arch OF datapath IS
+
+    COMPONENT pc_register
+        PORT (
+            reset : IN std_logic := '0';
+            clock : IN std_logic := '0';
+            d : IN std_logic_vector(31 DOWNTO 0) := (OTHERS => '0');
+            q : OUT std_logic_vector(31 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT i_cache
+        PORT (
+            instr_addr : IN std_logic_vector(4 DOWNTO 0) := (OTHERS => '0');
+            i_cache_instr_out : OUT std_logic_vector(31 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT next_address
+        PORT (
+            rt, rs : IN std_logic_vector(31 DOWNTO 0) := (OTHERS => '0');
+            pc : IN std_logic_vector(31 DOWNTO 0) := (OTHERS => '0');
+            target_address : IN std_logic_vector(25 DOWNTO 0) := (OTHERS => '0');
+            branch_type : IN std_logic_vector(1 DOWNTO 0) := (OTHERS => '0');
+            pc_sel : IN std_logic_vector(1 DOWNTO 0) := (OTHERS => '0');
+            next_pc : OUT std_logic_vector(31 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT two_input_mux IS
+        PORT (
+            s : IN std_logic;
+            in0 : IN std_logic_vector(31 DOWNTO 0);
+            in1 : IN std_logic_vector(31 DOWNTO 0);
+            mux_out : OUT std_logic_vector(31 DOWNTO 0);
+        );
+    END COMPONENT;
+    
+    COMPONENT regfile
+        PORT (
+            din : IN std_logic_vector(31 DOWNTO 0) := (OTHERS => '0');
+            reset : IN std_logic := '0';
+            clk : IN std_logic := '0';
+            write : IN std_logic := '0';
+            read_a : IN std_logic_vector(4 DOWNTO 0) := (OTHERS => '0');
+            read_b : IN std_logic_vector(4 DOWNTO 0) := (OTHERS => '0');
+            write_address : IN std_logic_vector(4 DOWNTO 0) := (OTHERS => '0');
+            out_a : OUT std_logic_vector(31 DOWNTO 0);
+            out_b : OUT std_logic_vector(31 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT alu
+        PORT (
+            -- two input operands x and y both 32-bits  
+            x, y : IN std_logic_vector(31 DOWNTO 0) := (OTHERS => '0');
+
+            -- 0 = add, 1 = sub
+            add_sub : IN std_logic := '0';
+
+            -- 00 = AND, 01 = OR, 10 = XOR, 11 = NOR
+            logic_func : IN std_logic_vector(1 DOWNTO 0) := (OTHERS => '0');
+
+            -- 00 = lui, 01 = setlessthan0, 10 = arith, 11 = logic
+            func : IN std_logic_vector(1 DOWNTO 0) := (OTHERS => '0');
+
+            output : OUT std_logic_vector(31 DOWNTO 0);
+            overflow : OUT std_logic;
+            zero : OUT std_logic
+        );
+    END COMPONENT;
+
+    COMPONENT d_cache
+        PORT (
+            clock : IN std_logic := '0';
+            data_write : IN std_logic := '0';
+            reset : IN std_logic := '0';
+            addr : IN std_logic_vector(4 DOWNTO 0) := (OTHERS => '0');
+            d_in : IN std_logic_vector(31 DOWNTO 0) := (OTHERS => '0');
+            d_out : OUT std_logic_vector(31 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    -- internal signal declarations
+
+    -- pc signals
+    SIGNAL next_pc : std_logic_vector(31 DOWNTO 0);
+END datapath_arch;
---
+++ results/lab4/code/i_cache.vhdl
@@ -0,0 +1,40 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.std_logic_unsigned.ALL;
+
+ENTITY i_cache IS
+    PORT (
+        instr_addr : IN std_logic_vector(4 DOWNTO 0);
+        i_cache_instr_out : OUT std_logic_vector(31 DOWNTO 0)
+    );
+END i_cache;
+
+ARCHITECTURE i_cache_arch OF i_cache IS
+
+BEGIN
+
+    -- eg. program
+    -- 00000 addi r1, r0, 1 ; r1 = r0 + 1 = 0 + 1
+    -- 00001 addi r2, r0, 2 ; r2 = r0 + 2 = 0 + 2
+    -- 00010 there: add r2, r2, r1 ; r2 = r2 + r1 = r2 + 1
+    -- 00011 j there ; goto label there
+
+    CASE instr_addr IS
+            -- addi r1, r0, 1
+        WHEN "00000" =>
+            i_cache_instr_out <= "00100000000000010000000000000001";
+            --addi r2, r0, 2
+        WHEN "00001" =>
+            ic_out <= "00100000000000100000000000000010";
+            -- add r2, r2, r1
+        WHEN "00010" =>
+            ic_out <= "00000000010000010001000000100000";
+            -- jump 00010
+        WHEN "00011" =>
+            ic_out <= "00001000000000000000000000000010";
+            -- don’t care
+        WHEN OTHERS =>
+            ic_out <= (OTHERS => '0');
+    END CASE;
+
+END i_cache_arch;
